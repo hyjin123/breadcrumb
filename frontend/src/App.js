193e739efc8698aaddc923f3611bc75572e9113a
Sean <seanhoyeonjin@gmail.com>
2022-12-02 14:37:38 -0500

    project set up

---
+++ frontend/src/App.js
@@ -0,0 +1,61 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  // this is the data that needs to be passed down to the front end
+  let root = {
+    type: "dir",
+    children: {
+      home: {
+        type: "dir",
+        children: {
+          myname: {
+            type: "dir",
+            children: {
+              "filea.txt": {
+                type: "file",
+              },
+              "fileb.txt": {
+                type: "file",
+              },
+              projects: {
+                type: "dir",
+                children: {
+                  mysupersecretproject: {
+                    type: "dir",
+                    children: {
+                      mysupersecretfile: {
+                        type: "file",
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
